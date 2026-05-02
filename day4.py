b069fce764f3b4c2b93190368bfa640dbed6d4b4
sunmingjia88-creator <sunmingjia88@gmail.com>
2026-05-02 15:37:16 +0800

    Update day4.py

--- day4.py
+++ day4.py
@@ -17,7 +17,7 @@ uploaded_image = st.file_uploader("Upload an image",
 # Display image with spinner
 if uploaded_image is not None:
     with st.spinner("Loading image..."):
-        time.sleep(1)  # Simulate a delay
+        time.sleep(5)  # Simulate a delay
         image = Image.open(uploaded_image)
         st.image(image, caption="Uploaded Image", use_column_width=True)
 
